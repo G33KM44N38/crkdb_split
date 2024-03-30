ed49f972e9346cb4c110304841898f7965a9559b
G33KM44N38 <mayanga.kylian@gmail.com>
2024-03-30 14:53:10 +0100

    feat: Removed IGNORE_MOD_TAP_INTERRUPT definition for clarity

--- config.h
+++ config.h
@@ -1,11 +1,9 @@
 #pragma once
 
-
-
 #define MASTER_LEFT
 
 /* #define QUICK_TAP_TERM 0 */
 #define TAPPING_TERM 125
-#define IGNORE_MOD_TAP_INTERRUPT
+// #define IGNORE_MOD_TAP_INTERRUPT
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
